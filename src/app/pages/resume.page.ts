5ba457dfd64efe340186045c214872de03a76a83
Logesh Kannan <logeshkannanengr@gmail.com>
2025-04-25 00:48:24 +0530

    fix: resume ui changes

--- src/app/pages/resume.page.ts
+++ src/app/pages/resume.page.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <div class="resume">
-      <p class="text-base font-medium">Resume</p>
+      <p class="text-base font-medium text-center mb-5">Resume</p>
       <iframe src="https://drive.google.com/file/d/13LgPJU1JoAS0uDuadG26ZDC3TMI3UjFh/preview" width="720" height="1080" allow="autoplay"></iframe>
     </div>
   `
