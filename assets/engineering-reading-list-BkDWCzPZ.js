26d990a90526836a4b22c73d9dfa28695dab0dea
logeshkannan96 <logeshkannan96@users.noreply.github.com>
2025-04-24 19:19:39 +0000

    deployed via Analog Publish Github Pages 🚀 for 5ba457dfd64efe340186045c214872de03a76a83

---
+++ assets/engineering-reading-list-BkDWCzPZ.js
@@ -0,0 +1,35 @@
+const e=`---
+title: Engineering Reading List
+slug: engineering-reading-list
+date: 15 April, 25
+---
+
+<p>Here’s a curated list of websites, blogs and articles worth keeping an eye on for their innovation, utility, or overall impact. This list will evolve over time as I discover more.</p>
+<h2 id="blogs-and-articles">Blogs and articles</h2>
+<ul>
+<li><a href="https://www.lydiahallie.com/blog/event-loop">Javascript event loop</a></li>
+<li><a href="https://blog.logrocket.com/rate-limiting-node-js/">Rate limiting in Node.js</a></li>
+</ul>
+<h2 id="websites">Websites</h2>
+<ul>
+<li><a href="https://landscape.cncf.io/">CNCF Landscape</a></li>
+<li><a href="https://zerodha.tech/blog/">Zerodha&#39;s Tech Blog</a></li>
+<li><a href="https://blog.cloudflare.com">Cloudflare&#39;s Blog</a></li>
+<li><a href="https://discord.com/blog/">Discord&#39;s Blog</a></li>
+<li><a href="https://engineering.fb.com">Facebook&#39;s blog</a></li>
+<li><a href="https://github.blog/category/engineering/">Github&#39;s Blog</a></li>
+<li><a href="https://netflixtechblog.medium.com">Netflix Technology Blog</a></li>
+</ul>
+<h2 id="software-practices">Software Practices</h2>
+<ul>
+<li><a href="https://www.joelonsoftware.com/2000/08/09/the-joel-test-12-steps-to-better-code">12 Steps to better code</a></li>
+<li><a href="https://12factor.net/">The 12 factor app</a></li>
+<li><a href="https://vadimkravcenko.com/shorts/things-they-didnt-teach-you/">Things they didn&#39;t teach you in tech</a></li>
+<li><a href="https://faculty.washington.edu/ajko/papers/Li2019WhatDistinguishesEngineers.pdf">What distinguishes great software engineers?</a></li>
+<li><a href="https://mtlynch.io/code-review-love/">How to make your code reviewer fall in love with you</a></li>
+</ul>
+<h2 id="uncategorized">Uncategorized</h2>
+<ul>
+<li><a href="https://stackoverflow.com/tags/timezone/info">Timezones</a></li>
+</ul>
+`;export{e as default};
